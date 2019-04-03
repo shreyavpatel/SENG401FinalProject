d3adb2e0f98bfbca9ff6dd689e0eb8bb104f57c2
Dylan <dylan.gordon.10@gmail.com>
2019-04-03 07:52:46 -0600

    Added authentication

--- BrainCandy/resources/views/youtube/index.blade.php
+++ BrainCandy/resources/views/youtube/index.blade.php
@@ -2,11 +2,12 @@
 
 <head>
 	<title>Youtube API search</title>
-	<script src="http://code.jquery.com/jquery-3.3.1.min.js"
+</head>
+
+<script src="http://code.jquery.com/jquery-3.3.1.min.js"
 	      integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
 	      crossorigin="anonymous">
 	</script>
-</head>
 
 <body>
 	<div>
--- BrainCandy/routes/web.php
+++ BrainCandy/routes/web.php
@@ -17,3 +17,6 @@ Route::get('/', function () {
 
 
 Route::resource('youtube', 'YoutubeController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
