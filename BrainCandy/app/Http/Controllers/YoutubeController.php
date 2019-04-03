61d6b6e07c25e80e9249d1819fa277d195f0e5d5
Dylan <dylan.gordon.10@gmail.com>
2019-04-03 00:07:46 -0600

    Made a demo of how to use youtube API

---
+++ BrainCandy/app/Http/Controllers/YoutubeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Route;
+
+class YoutubeController extends Controller
+{
+
+
+    public function index(){
+
+    	return view('youtube.index');
+    	// GET https://www.googleapis.com/youtube/v3/search
+
+    	// Create GET request to use books api
+        $request = Request::create('https://www.googleapis.com/youtube/v3/search/?part=snippet&maxResults=25&q=surfing&key=AIzaSyDsbrC-_RBZ28drg6FNV01xjvJ_QkYHZvE', 'GET');
+
+        // dispatch request to API
+        $response = Route::dispatch($request);
+
+        dd($response);
+
+        //decode the response json into arrays (true argument)
+        $data = json_decode($response->content(), true);
+        dd($data);
+
+    	return "nothing";
+    }
+}
---
+++ BrainCandy/resources/views/youtube/index.blade.php
@@ -0,0 +1,58 @@
+<html>
+
+<head>
+	<title>Youtube API search</title>
+	<script src="http://code.jquery.com/jquery-3.3.1.min.js"
+	      integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+	      crossorigin="anonymous">
+	</script>
+</head>
+
+<body>
+	<div>
+		Enter search item: <br>
+		<input type="text" id="searchParameter">
+		<input type="submit" id="search">
+	</div>
+
+	<div id="testResultArea">
+
+
+	</div>
+</body>
+
+</html>
+
+
+
+<script>
+         $(document).ready(function(){
+         	$("#search").click(function(){
+               $.ajaxSetup({
+                  headers: {
+                      'X-CSRF-TOKEN': $('meta[name="csrf_token"]').attr('content')
+                  }
+              });
+               console.log("https://www.googleapis.com/youtube/v3/search/?part=snippet&maxResults=25&key=AIzaSyDsbrC-_RBZ28drg6FNV01xjvJ_QkYHZvE&q=" + $("#searchParameter").val());
+               $.ajax({
+                  url: "https://www.googleapis.com/youtube/v3/search/?part=snippet&maxResults=25&key=AIzaSyDsbrC-_RBZ28drg6FNV01xjvJ_QkYHZvE&q=" + $("#searchParameter").val(),
+                  method: 'GET',
+                  data: {
+               
+                  },
+                  success: function(result){
+
+                  	// console.log(result['items']['snippet']);
+                  		$("#testResultArea").html('');
+					    $.each(result['items'], function (index, value) {
+					    	console.log(value);
+					        $("#testResultArea").append (
+							 "  <img src="+ value['snippet']['thumbnails']['default']['url']+" class='img-fluid'>" + 
+							 "<h3> " + value['snippet']['title'] + "</h3>" + 
+							 "<h5> Channel: " + value['snippet']['channelTitle'] + "</h5>" +
+							"<hr>");
+					});
+                  }});
+           });
+            });
+</script>
