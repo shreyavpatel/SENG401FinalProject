d858ef0478744af0627f378a9cf461c46ef89d43
Alexa Astorino <alexa.astorino@gmail.com>
2019-04-04 10:04:10 -0600

    added temporary symbols

--- BrainCandy/resources/views/home.blade.php
+++ BrainCandy/resources/views/home.blade.php
@@ -80,7 +80,7 @@
                     <a href="/users/edit/{{Auth()->User()->id}}">Edit Your Flavor Profile</a>
                     <img class="link_logo" src="lolipop.png"> 
                     <a href="/feed"> Feed </a>
-                    <img class="link_logo" src="jawbreaker.png"> 
+                    <img class="link_logo" src="mouth.png"> 
                     <a href="#"> Jaw Breakers </a>
                     <img class="link_logo" src="jawbreaker.png"> 
                 </div>
