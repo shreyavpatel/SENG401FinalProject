38f6844fd2197a3b6e591afead098b9d7a52354a
Ross Bartlett <rossjbartlett@gmail.com>
2019-04-03 18:05:52 -0600

    changes to home

--- BrainCandy/resources/views/home.blade.php
+++ BrainCandy/resources/views/home.blade.php
@@ -1,5 +1,47 @@
 @extends('layouts.app')
+<style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
 
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
 @section('content')
 @if (session('success'))
                         <div class="alert alert-success" role="alert">
@@ -21,9 +63,19 @@
         </div>
     </div>
 
-    <div class="links">
-        <a href="/users/edit/{{Auth()->User()->id}}">My Flavor Profile</a>
+
+    <div class="flex-center position-ref ">
+            <div class="content">
+                <div class="title m-b-md">
+                    BrainCandy
+                </div>
+
+                <div class="links">
+                    <a href="/users/edit/{{Auth()->User()->id}}">Edit Flavor Profile</a>
+                </div>
+            </div>
     </div>
+
 
 </div>
 
