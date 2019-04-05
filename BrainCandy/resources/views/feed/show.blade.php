8931270c5c57248d4b2325e1b3f8e89d3f496908
Ross Bartlett <rossjbartlett@gmail.com>
2019-04-05 11:04:23 -0600

    uncomment extra jquery export in feed.show

--- BrainCandy/resources/views/feed/show.blade.php
+++ BrainCandy/resources/views/feed/show.blade.php
@@ -1,12 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<meta name="csrf-token" content="{{ csrf_token() }}">
-<script src="http://code.jquery.com/jquery-3.3.1.min.js"
-          integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-          crossorigin="anonymous"></script>
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
 <script>
 	$(document).ready(function(){
 		var options = [];
